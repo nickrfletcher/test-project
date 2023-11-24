d86753be91cd84a9c87ce7d6fe848f1445492f52
Nick Fletcher <nickrfletcher@gmail.com>
2023-11-24 16:23:07 -0500

    Experiments with Git Copilot and Ruby.

--- rubytest.rb
+++ rubytest.rb
@@ -1,5 +1,21 @@
 #!/usr/bin/env ruby
 
-puts "Hello, Ruby!"
+# Define a variable
+name = "John"
+
+# Use conditional statements
+if name == "John"
+    puts "Hello, John!"
+else
+    puts "Hello, stranger!"
+end
+
+# Use a loop
+5.times do |i|
+    puts "Count: #{i+1}"
+end
 
 # Path: rubytest.rb
+
+
+
